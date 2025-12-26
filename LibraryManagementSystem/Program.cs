0e5bd1129ad7f9eaed9fe2dd5cc957dd9ad896dc
M.Burakhan Özçelik <burakhanozcelik2@hotmail.com>
2025-12-26 05:01:58 +0300

    using (var scope = app.Services.CreateScope()) {     var db = scope.ServiceProvider.GetRequiredService<LibraryManagementSystem.Data.ApplicationDbContext>();     db.Database.Migrate(); }

--- LibraryManagementSystem/Program.cs
+++ LibraryManagementSystem/Program.cs
@@ -18,5 +18,10 @@ app.UseRouting();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<LibraryManagementSystem.Data.ApplicationDbContext>();
+    db.Database.Migrate();
+}
 
 app.Run();
